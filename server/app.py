00e09fe1cec04f2b679f1e60fb87495ab6f8db93
Jack Kolb <kolb@gatech.edu>
2021-02-07 16:22:31 -0500

    Added server -> UI image stream

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import rospy
 
 import base64
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Server is online"
+    return render_template("index.html")
 
 @app.route("/cam", methods=["GET"])
 def cam():
@@ -24,10 +24,11 @@ def cam():
     
     # return the image
     # adapted from https://stackoverflow.com/questions/3715493/
-    filename = "img/cam" + str(cam) + ".png"
+    filename = "static/img/cams/cam" + str(cam) + ".png"
     with open(filename, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
-
+    
     return encoded_string
+
 
 app.run(host="0.0.0.0", port=5000)
---
+++ server/static/js/inputUtils.js
@@ -0,0 +1,44 @@
+// handles the canvas mouse events
+function canvasMouseHandler(event) {
+    // if the mouse click is outside the range of the canvas, ignore it
+    if (event.originalTarget != document.getElementById("uimap-canvas") && event.target != document.getElementById("uimap-canvas")) {
+        return;
+    }
+
+    var canvasX = event.clientX - (event.originalTarget ? event.originalTarget.offsetLeft : event.target.offsetLeft);
+    var canvasY = event.clientY - (event.originalTarget ? event.originalTarget.offsetTop : event.target.offsetTop);
+    var canvasClick = [canvasX, canvasY];
+
+    clickedVehicle = false;
+
+    // check if any vehicles were clicked
+    for (i in uiMap.uiObjects) {
+        if (uiMap.distance(canvasClick, [uiMap.uiObjects[i].x, uiMap.uiObjects[i].y]) < uiMap.uiObjects[i].scale * .86) {
+            console.log("Selected vehicle");
+            uiMap.selectedObject = uiMap.uiObjects[i];
+            clickedVehicle = true;
+            break;
+        }
+    }
+
+    // if no vehicles were clicked, add a waypoint if applicable
+    if (!clickedVehicle && uiMap.selectedObject != null && event.button == 0) {
+        uiMap.selectedObject.waypoints.push([canvasX / uiMap.mapCanvas.width, canvasY / uiMap.mapCanvas.height]);
+        console.log(" added waypoint, ", uiMap.selectedObject.waypoints);
+    }
+}
+
+function canvasKeypressHandler(event) {
+    console.log(event)
+    // if an space key and an object is selected, remove the last waypoint
+    if (event.key == " " && uiMap.selectedObject != null) {
+        console.log("Removed waypoint");
+        uiMap.selectedObject.waypoints.pop();
+    }
+
+    // if a enter key and an object is selected, remove the last waypoint
+    if (event.key == "Enter" && uiMap.selectedObject != null) {
+        console.log("Deselected vehicle");
+        uiMap.selectedObject = null;
+    }
+}
