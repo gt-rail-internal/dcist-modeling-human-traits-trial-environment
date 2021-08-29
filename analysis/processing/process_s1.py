d8cb0d002a2d0904fed25f18d186c6fa7638ac1e
Jack Kolb <jack@jackkolb.com>
2021-08-28 22:56:32 -0400

    debug

--- analysis/processing/process_s1.py
+++ analysis/processing/process_s1.py
@@ -79,8 +79,10 @@ def get_s1_data(path):
                     response += "\n" + "  Stage 1 duration " + str(end_time - start_time)
                     if sum([float(x) for x in distance_traveled]) > 0:
                         s1_scores[p] = int(100000 * cache_collected / sum([float(x) for x in distance_traveled]))
+                        print("!!!", p, s1_scores[p])
                     else:
                         s1_scores[p] = 0
+                        print("---", p, s1_scores[p])
 
                     break
 
